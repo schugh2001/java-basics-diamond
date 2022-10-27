e9117ca77d7698af43eeb580b522a9faee8a736b
schugh2001 <67055559+schugh2001@users.noreply.github.com>
2022-10-27 12:25:22 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class diamond
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        diamond(n);
+    }
+    public static void diamond(int n)
+    {
+        //1st half
+        for(int i=1;i<=n;i++)
+        {
+            //spaces 
+            for(int j=1;j<=(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            //stars
+            for(int k=1;k<=((2*i)-1);k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //2nd half
+        for(int i=n;i>=1;i--)
+        {
+            //spaces 
+            for(int j=1;j<=(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            //stars
+            for(int k=1;k<=((2*i)-1);k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
